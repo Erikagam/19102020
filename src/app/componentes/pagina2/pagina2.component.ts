7b14178f6ee1018a75c5e8b3c97eeac84a0c2c3c
Erikagam <68310719+Erikagam@users.noreply.github.com>
2020-11-11 21:05:29 -0600

    "faltan botones descuento y pagos"

--- src/app/componentes/pagina2/pagina2.component.ts
+++ src/app/componentes/pagina2/pagina2.component.ts
@@ -40,6 +40,32 @@ export class Pagina2Component implements OnInit {
     console.log("Básica :"+' '+this.basica+' '+this.statusB)
   }
 
+//   descALTO=false;
+//   descMEDIO=false;
+//   descBAJO=false;
+//   statusDA = "NoSelected";
+//   statusDM= "NoSelected";
+//   statusDB= "NoSelected";
+//   Descuentoalto( ) {
+//     // tiene selected this.statusDA
+//     this. descALTO = !this. descALTO;
+//     this.statusDA = this. descALTO ? "Selected"  :"NoSelected";
+//     console.log("Descuento Alto:"+' '+this.descALTO+' '+this.statusDA)
+//   }
+//  Descuentomedio( ) {
+//     // tiene selected this.statusDM
+//     this. descMEDIO = !this. descMEDIO;
+//     this.statusDM= this. descMEDIO ? "Selected" : "NoSelected";
+//     console.log("Descuento Medio :"+' '+this.descMEDIO+' '+this.statusDM)
+//   }
+//   Descuentobajo( ) {
+//     // tiene selected this.statusDB
+//     this. descBAJO = !this. descBAJO;
+//     this.statusDB= this. descBAJO ? "Selected" : "NoSelected" ;
+//     console.log("Descuento Bajo :"+' '+this.descBAJO+' '+this.statusDB)
+//   }
+
+
   constructor() { }
 
   ngOnInit(): void {
