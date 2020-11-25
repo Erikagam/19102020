e2032dc12520e021ef53e591795d3d435b8984df
Erikagam <68310719+Erikagam@users.noreply.github.com>
2020-11-25 12:39:29 -0600

    "ya sirve routing"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import {LayoutComponent} from './layout/layout.component'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes//header/header.component';
@@ -10,6 +10,7 @@ import { Pagina1Component } from './componentes/pagina1/pagina1.component';
 import { InfoaseguradoComponent } from './componentes/pagina1/infoasegurado/infoasegurado.component';
 import {InfovehiculoComponent} from './componentes/pagina1/infovehiculo/infovehiculo.component';
 import {Pagina2Component } from './componentes/pagina2/pagina2.component'
+import { from } from 'rxjs';
 @NgModule({
   declarations: [AppComponent,
   HeaderComponent,
@@ -17,7 +18,8 @@ import {Pagina2Component } from './componentes/pagina2/pagina2.component'
   Pagina1Component,
   InfoaseguradoComponent,
   InfovehiculoComponent,
-  Pagina2Component
+  Pagina2Component,
+  LayoutComponent
 ],
   imports: [
     FormsModule,
